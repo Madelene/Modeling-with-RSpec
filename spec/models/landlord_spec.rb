abbabed72ce57c6aa075a80a025713f4d3bd884a
Madelene Campos <madelenecampos@gmail.com>
2015-11-11 13:34:54 -0500

    Wrote first test in landlord_spec.rb file

--- spec/models/landlord_spec.rb
+++ spec/models/landlord_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Landlord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:landlord) { Landlord.new }
+
+  it { is_expected.to have_many(:properties) }
 end
